febd2c9759011fa0502cf9259349b9ad83e51679
brad-soderman <brad.soderman@gmail.com>
2014-04-03 14:24:14 -0600

    I added some functionality to updateItemInfo(), updatePatronInfo(), and modified the btnAdvancedClicked().

--- LibraryProject/LibraryProject/SubGUICode.cs
+++ LibraryProject/LibraryProject/SubGUICode.cs
@@ -27,14 +27,68 @@ namespace LibraryProject
             return items[selItem];
         }
 
+        //Brad
+        //Brad Notes: 
+        //   first and last in the patron class needs to be made public so I can access them.
+        //   In the Adult and Child classes, there needs to be a reference to the array that holds that Objecs items.
+
         private void updatePatronInfo()
         {
-            
+            txtBPatronName.Text = curPatron.first + " " + curPatron.last;
+
+            //check the type of the patron
+            if (curPatron is Adult)
+            {
+                txtBPatronType.Text = "Adult";
+            }
+            else if (curPatron is Child)
+            {
+                txtBPatronType.Text = "Child";
+            }
+
+            //I am unsure how to access the child classes (Adult, Child) from an instance of the parent class (Patron)
+            //var childPatron = (Child)curPatron;
         }
 
+        //Brad
+        //Notes:
+        //  title needs to be made public so it can be accessed.
+        //  I am unsure how to access the child class's data through an instance of the parent class
+        //  status of the object (checked out or checked in) maybe should be in the Item class since all items will need it.
         private void updateItemInfo()
         {
-            
+            txtBItemTitle.Text = curItem.title;
+            txtBItemStatus.Text = curItem.status;
+
+            //check type of item
+            if (curItem is AdultBook)
+            {
+                txtBItemType.Text = "Adult Book";
+                txtBItemCheckoutLen.Text = "2 Weeks";
+                dateDue.Value = curItem.dueDate;
+              
+            }
+            else if (curItem is ChildBook)
+            {
+                txtBItemType.Text = "Child Book";
+                txtBItemCheckoutLen.Text = "1 Week";
+                dateDue.Value = curItem.dueDate;
+              
+            }
+            else if (curItem is DVD)
+            {
+                txtBItemType.Text = "DVD";
+                txtBItemCheckoutLen.Text = "2 days";
+                dateDue.Value = curItem.dueDate;
+            }
+            else if (curItem is VHS)
+            {
+                txtBItemType.Text = "VHS";
+                txtBItemCheckoutLen.Text = "3 days";
+                dateDue.Value = curItem.dueDate;
+            }
+
+           
         }
 
         private void updateItemsCheckOut()
@@ -106,9 +160,10 @@ namespace LibraryProject
         {
             //Increment the date by one
             var today = dateToday.Value.AddDays(1);
+            int day365 = today.DayOfYear;
             foreach (Item x in checkedOut)
             {
-                if (x.dueDate < today)
+                if (x.dueDate.DayOfYear > day365)
                 {
                     lstItemsOverdue.Items.Add(x);
                 }
