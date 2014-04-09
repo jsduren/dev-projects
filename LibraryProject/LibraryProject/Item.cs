7f8f97b7b877f09f7c1a2670a98ceafb6d4467b5
Austin Barnes <austin.barnes04@gmail.com>
2014-04-08 20:11:25 -0600

    Checkout Method done for Items

--- LibraryProject/LibraryProject/Item.cs
+++ LibraryProject/LibraryProject/Item.cs
@@ -4,16 +4,16 @@ using System;
 class Item 
 {
 	//maybe put different types of items as a category as a private 
-	private string title;
-	private string whoCheckedOut;
-	private bool checkout = false;
+	public string title;
+	public string whoCheckedOut;
+	public bool checkout;
 	public DateTime checkDate;
     public DateTime dueDate; //I added this so I could compare the dates. -Brad
 
     public bool checkOutState; //Added this so I could test for current checkout state -Josh
 
 
-	public Item(string _title, bool check, string _who = "", DateTime _checkDate = DateTime.Now)
+	public Item(string _title, bool check = false, string _who = "", DateTime _checkDate = DateTime.Now)
 	{
 		checkedout = check;
 		title = _title;
@@ -22,25 +22,23 @@ class Item
 	}
     // added the function brackets to be able to compile program. -Josh
     public virtual void checkout(){}
-
     // added the function brackets to be able to compile program. -Josh
-    public void checkin()
-    {
-	}
-
+    public void checkin(){}
     // added the function brackets to be able to compile program. -Josh
-    public virtual int days()
-    {
-    }
-	
-	public bool isCheckedOut(){}
+    public virtual int days(){}
+	public bool isCheckedOut(){ return checkout;}
 }
 
 //child Book
 class ChildBook: Item
 {
 	private static const int MAX = 7; //1 week 
-	public override void checkout(){}
+	public override void checkout(string _who)
+	{
+		whoCheckOut = _who;
+		checkDate = DateTime.Now;
+		dueDate = checkDate.AddDays(MAX);
+	}
 	public override int days() {	return MAX;	}
 	ChildBook():Item{}
 }
@@ -49,11 +47,13 @@ class ChildBook: Item
 class AdultBook: Item
 {
 	private static const int MAX = 14; //2 weeks
-	public override void checkout(){}
-	public override int days()
-    {
-		return MAX;
+	public override void checkout()
+	{
+		whoCheckOut = _who;
+		checkDate = DateTime.Now;
+		dueDate = checkDate.AddDays(MAX);
 	}
+	public override int days(){	return MAX;	}
 	public AdultBook():Item{}
 }
 
@@ -61,11 +61,13 @@ class AdultBook: Item
 class DVD: Item 
 {
 	private static const int MAX = 2;
-	public override void checkout(){}
-	public override int days()
-    {
-		return MAX;
+	public override void checkout()
+	{
+		whoCheckOut = _who;
+		checkDate = DateTime.Now;
+		dueDate = checkDate.AddDays(MAX);
 	}
+	public override int days(){	return MAX;	}
 	public DVD():Item{}
 }
 
@@ -73,10 +75,12 @@ class DVD: Item
 class VHS: Item
 {
 	private static const int MAX = 3; 
-	public override void checkout(){}
-	public override int days()
-    {
-		return MAX;
+	public override void checkout()
+	{
+		whoCheckOut = _who;
+		checkDate = DateTime.Now;
+		dueDate = checkDate.AddDays(MAX);
 	}
+	public override int days(){	return MAX;	}
 	public VHS():Item{}
 }
