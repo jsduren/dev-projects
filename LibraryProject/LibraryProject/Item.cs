201e43a5d8ca41530a5f74e233dbdb49eff9a4fe
Austin Barnes <austin.barnes04@gmail.com>
2014-04-08 19:56:24 -0600

    Refinement of Item Classes

--- LibraryProject/LibraryProject/Item.cs
+++ LibraryProject/LibraryProject/Item.cs
@@ -3,48 +3,71 @@ using System;
 
 class Item 
 {
+	//maybe put different types of items as a category as a private 
 	private string title;
 	private string whoCheckedOut;
+	private bool checkout = false;
+	public DateTime checkDate;
     public DateTime dueDate; //I added this so I could compare the dates. -Brad
-	//timestamp of check out date
-	//due date 
 
+	public Item(string _title, bool check, string _who = "", DateTime _checkDate = DateTime.Now)
+	{
+		checkedout = check;
+		title = _title;
+		whoCheckedOut = _who;
+		checkDate = _checkDate;
+	}
     // added the function brackets to be able to compile program. -Josh
-    public void checkout()
-    {
-    }
+    public virtual void checkout(){}
 
     // added the function brackets to be able to compile program. -Josh
     public void checkin()
     {
-    }
+	}
 
     // added the function brackets to be able to compile program. -Josh
-    public void days()
+    public virtual int days()
     {
     }
+	
+	public bool isCheckedOut(){}
 }
 
 //child Book
 class ChildBook: Item
 {
 	private static const int MAX = 7; //1 week 
+	public override void checkout(){}
+	public override int days()
+    {
+    }
 }
 
 //Adult Book
 class AdultBook: Item
 {
 	private static const int MAX = 14; //2 weeks
+	public override void checkout(){}
+	public override int days()
+    {
+    }
 }
 
 //Dvd
 class DVD: Item 
 {
-	private static const int MAX = 2; 
+	private static const int MAX = 2;
+	public override void checkout(){}
+	public override int days()
+    {
+    }
 }
 
 //Vhs
 class VHS: Item
 {
 	private static const int MAX = 3; 
-}
+	public override void checkout(){}
+	public override int days()
+    {
+    }
