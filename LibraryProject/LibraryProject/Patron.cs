3a949f2106e1856abdb586a221d37641498fbbac
iconza98 <iconza98@gmail.com>
2014-04-15 11:55:12 -0600

    updated file class

--- LibraryProject/LibraryProject/Patron.cs
+++ LibraryProject/LibraryProject/Patron.cs
@@ -53,15 +53,9 @@ public class Child : Patron
     {
         //checks the type
 
-		if(Item is ChildBook && numberofItems < MAX)//**************************Fix!!!!!!!!!!!!!!!!!!1
-
-		if(current is ChildBook || numberofItems == MAX)
-<<<<<<< HEAD
-=======
-
->>>>>>> 2c97a88d6e25389fb69e8044d28780edb96b9400
+		if(current is ChildBook && numberofItems < MAX)//**************************Fix!!!!!!!!!!!!!!!!!!1
 		{
-			current.checkout(this.displayname());
+			current.checkout(this.displayName());
 			++numberofItems;
 			return true;
 		}
@@ -72,10 +66,6 @@ public class Child : Patron
 		}
 		else 
 		{
-<<<<<<< HEAD
-=======
-
->>>>>>> 2c97a88d6e25389fb69e8044d28780edb96b9400
 			throw new System.ArgumentException("Item is not a child book...", "current");
 			return false;
 		}
@@ -84,10 +74,7 @@ public class Child : Patron
             ++numberofItems;
             return true;
 		}	
-<<<<<<< HEAD
-=======
 
->>>>>>> 2c97a88d6e25389fb69e8044d28780edb96b9400
     }
 
     public override void checkin(ref Item current) //pass by reference item
