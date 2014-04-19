a183b92ef5a3c3d5ed4abc1679c24311a3f1f1b9
iconza98 <iconza98@gmail.com>
2014-04-18 22:09:13 -0600

    Test Class intial commit
    commit before any changes need to be adjusted

---
+++ LibraryProject/LibraryTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LibraryProject;
+using LibraryProject.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AdultCheckOut_MaxDaysCheck()
+        {
+            // arrange
+            int check = 14;
+            Adult person = new Adult();
+        }
+    }
+}
